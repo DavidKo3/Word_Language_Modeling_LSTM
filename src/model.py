fbda70b7d5ba3dacc326a0fb3495ce9fc2c02ffe
DavidKo3 <automorphicnew@gmail.com>
2018-02-22 16:51:55 +0900

    Remove comments

--- src/model.py
+++ src/model.py
@@ -120,14 +120,6 @@ class NestedLSTM(nn.Module):
         #c_0 = Variable(weight.new(self.nlayers, bsz, self.nhid).zero_())
         return (h_0, c_0, tild_c_0)
         #return (h_0, c_0)
-    """
-        if self.rnn_type == 'LSTM':
-            return (Variable(weight.new(self.nlayers, bsz, self.nhid).zero_()),
-                    Variable(weight.new(self.nlayers, bsz, self.nhid).zero_()))
-        else:
-            return Variable(weight.new(self.nlayers, bsz, self.nhid).zero_())
-
-    """
 
 class RNNModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
